6624d9e1c75cbbc65ee9cbb97fef87eaef4deefc
Giovani H. <113646706+1N-i@users.noreply.github.com>
2026-03-10 15:54:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 let btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9
-btn1 = btn3 = btn7 = btn9 = false
-btn2 = btn4 = btn6 = btn8 = "false"
-btn5 = "False"
+btn1 = btn2 = btn3 = btn4 = btn5 = btn6 = btn7 = btn8 = btn9 = false
 
 let player = "X"
 
@@ -14,35 +12,35 @@ let player = "X"
 */
 
 function checkVictory() {
-    if (btn1 === btn2 && btn2 === btn3){
+    if (btn1 === btn2 && btn2 === btn3 && btn3 !== false){
         const winner = document.getElementById("winText")
         winner.innerHTML = `Victory of "${player}"`
     }
-    if (btn4 === btn5 && btn5 === btn6){
+    if (btn4 === btn5 && btn5 === btn6 && btn6 !== false){
         const winner = document.getElementById("winText")
         winner.innerHTML = `Victory of "${player}"`
     }
-    if (btn7 === btn8 && btn8 === btn9){
+    if (btn7 === btn8 && btn8 === btn9 && btn9 !== false){
         const winner = document.getElementById("winText")
         winner.innerHTML = `Victory of "${player}"`
     }
-    if (btn1 === btn4 && btn4 === btn7){
+    if (btn1 === btn4 && btn4 === btn7 && btn7 !== false){
         const winner = document.getElementById("winText")
         winner.innerHTML = `Victory of "${player}"`
     }
-    if (btn2 === btn5 && btn5 === btn8){
+    if (btn2 === btn5 && btn5 === btn8 && btn8 !== false){
         const winner = document.getElementById("winText")
         winner.innerHTML = `Victory of "${player}"`
     }
-    if (btn3 === btn6 && btn6 === btn9){
+    if (btn3 === btn6 && btn6 === btn9 && btn9 !== false){
         const winner = document.getElementById("winText")
         winner.innerHTML = `Victory of "${player}"`
     }
-    if (btn1 === btn5 && btn5 === btn9){
+    if (btn1 === btn5 && btn5 === btn9 && btn9 !== false){
         const winner = document.getElementById("winText")
         winner.innerHTML = `Victory of "${player}"`
     }
-    if (btn3 === btn5 && btn5 === btn7){
+    if (btn3 === btn5 && btn5 === btn7 && btn7 !== false){
         const winner = document.getElementById("winText")
         winner.innerHTML = `Victory of "${player}"`
     }
@@ -69,7 +67,7 @@ function place1(){
 }
 
 function place2(){
-    if (btn2 === "false"){
+    if (btn2 === false){
         const play = document.getElementById("bt2")
         const text = document.getElementById("playerText")
         btn2 = player
@@ -87,28 +85,28 @@ function place3(){
 }
 
 function place4(){
-    if (btn4 === "false"){
+    if (btn4 === false){
         const play = document.getElementById("bt4")
         const text = document.getElementById("playerText")
-        btn4 = true
+        btn4 = player
         switchPlayer(play, text)
     }
 }
 
 function place5(){
-    if (btn5 === "False"){
+    if (btn5 === false){
         const play = document.getElementById("bt5")
         const text = document.getElementById("playerText")
-        btn5 = true
+        btn5 = player
         switchPlayer(play, text)
     }
 }
 
 function place6(){
-    if (btn6 === "false"){
+    if (btn6 === false){
         const play = document.getElementById("bt6")
         const text = document.getElementById("playerText")
-        btn6 = true
+        btn6 = player
         switchPlayer(play, text)
     }
 }
@@ -117,16 +115,16 @@ function place7(){
     if (btn7 === false){
         const play = document.getElementById("bt7")
         const text = document.getElementById("playerText")
-        btn7 = true
+        btn7 = player
         switchPlayer(play, text)
     }
 }
 
 function place8(){
-    if (btn8 === "false"){
+    if (btn8 === false){
         const play = document.getElementById("bt8")
         const text = document.getElementById("playerText")
-        btn8 = true
+        btn8 = player
         switchPlayer(play, text)
     }
 }
@@ -135,7 +133,7 @@ function place9(){
     if (btn9 === false){
         const play = document.getElementById("bt9")
         const text = document.getElementById("playerText")
-        btn9 = true
+        btn9 = player
         switchPlayer(play, text)
     }
 }
